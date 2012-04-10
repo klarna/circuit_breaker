90d2b46f7c28d9d3799a204d8e0a4f4e6c9841a9
Christian Rennerskog <christian.r@klarna.com>
2012-04-10 22:51:31 +0200

    Add circuit_breaker flags

--- include/circuit_breaker.hrl
+++ include/circuit_breaker.hrl
@@ -7,6 +7,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%_* Defines ==========================================================
+%% #circuit_breaker.flags
+-define(CIRCUIT_BREAKER_OK,           0).
+-define(CIRCUIT_BREAKER_WARNING,      (1 bsl 0)).
+-define(CIRCUIT_BREAKER_BLOCKED,      (1 bsl 1)).
+-define(CIRCUIT_BREAKER_CALL_TIMEOUT, (1 bsl 2)).
+-define(CIRCUIT_BREAKER_TIMEOUT,      (1 bsl 3)).
+-define(CIRCUIT_BREAKER_ERROR,        (1 bsl 4)).
 
 %%%_* Emacs ============================================================
 %%% Local Variables:
